7bbe3ab4785cd2f25454e9969fae8196c1c5390b
blizzard190 <22256@ma-web.nl>
2020-06-10 12:46:30 +0200

    Added object placement in AR

---
+++ Assignment/Assets/Script/TapToPlaceObject.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine.XR.ARFoundation;
+using UnityEngine.XR.ARSubsystems;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TapToPlaceObject : MonoBehaviour
+{
+    public GameObject placableObject;
+       
+   private ARRaycastManager _raycastManager;
+   private Pose _placementPose;
+   private bool _placementPoseIsValid = false;
+   void Start() 
+   {
+       _raycastManager = FindObjectOfType<ARRaycastManager>();
+   }
+   
+   void Update()
+   {
+       if (Input.touchCount > 0 && Input.GetTouch(0).phase == TouchPhase.Began || Input.GetKeyDown(KeyCode.A))
+       {
+           PlaceObject();
+       }
+   }
+
+   private void PlaceObject()
+   {
+       Vector2 screenPosition = Camera.main.ViewportToScreenPoint(Input.GetTouch(1).position);
+       var hits = new List<ARRaycastHit>();
+       _raycastManager.Raycast(screenPosition, hits,TrackableType.Planes);
+       _placementPoseIsValid = hits.Count > 0;
+       if (_placementPoseIsValid)
+       {
+           _placementPose = hits[0].pose;
+   
+           var cameraForward = Camera.main.transform.forward;
+           var cameraBearing = new Vector3(cameraForward.x,0, cameraForward.z).normalized;
+           _placementPose.rotation = Quaternion.LookRotation(cameraBearing);
+
+           Instantiate(placableObject, _placementPose.position, _placementPose.rotation);
+       }
+   }
+}
